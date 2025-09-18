2030ab371bbe4291b75701227d412c44dd7ab7a9
rimpa kasundi <rimpakasundi@gmail.com>
2025-09-18 23:42:58 +0530

    Added click event handlers with inline, named, and arrow functions for interactive buttons

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,28 @@
 import "./App.css";
 
 function App() {
+  function handleClick() {
+    alert("I am clicked.");
+  }
+  const handleClick3 = () => {
+    alert("clicked 3");
+  };
+
   return (
     <>
-      <h1>Vite + React</h1>
+      <h3>Vite + React</h3>
+      {/* <button onClick="handleClick()">Click Me</button> */}
+      <button onClick={handleClick}>Click Me</button>
+      <button
+        onClick={function handleClick2() {
+          alert("clicked 2");
+        }}
+      >
+        Click Me 2{" "}
+      </button>
+
+      <button onClick={handleClick3}>Click Me 3</button>
+      <button onClick={() => alert("click 4")}>Click Me 4</button>
     </>
   );
 }
