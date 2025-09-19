394ac126e5010d039483bfa8d9d0b6833bcb4098
rimpa kasundi <rimpakasundi@gmail.com>
2025-09-19 17:37:04 +0530

    Add boundary counters and 50+ score indicator to batsman component

--- src/batsman.jsx
+++ src/batsman.jsx
@@ -1,7 +1,9 @@
 import { useState } from "react";
 
 export default function Batsman() {
-  let [runs, setRuns] = useState(0);
+  const [runs, setRuns] = useState(0);
+  const [four, setfour] = useState(0);
+  const [sixes, setSixes] = useState(0);
 
   const handleSingle = () => {
     const updatedRuns = runs + 1;
@@ -9,17 +11,30 @@ export default function Batsman() {
   };
   const handleFour = () => {
     const updatedRuns = runs + 4;
+    const updatedFour = four + 1;
+    setfour(updatedFour);
     setRuns(updatedRuns);
   };
 
   const handleSix = () => {
     const updatedRuns = runs + 6;
+    const updatedSixes = sixes + 1;
+    setSixes(updatedSixes);
     setRuns(updatedRuns);
   };
 
   return (
     <div>
       <h1>Player : MSD</h1>
+      <p>
+        <small>Four:{four} </small>
+      </p>
+      <br></br>
+      <p>
+        <small>Six:{sixes} </small>
+      </p>{" "}
+      <br></br>
+      {runs > 50 && <p>Your Score :50+</p>}
       <h3>Score: {runs} </h3>
       <button onClick={handleSingle}>Single Run</button>
       <button onClick={handleFour}> It's Four</button>
