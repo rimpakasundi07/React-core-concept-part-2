d3cd27d4d2112b692abe8f671aa37512773c9485
rimpa kasundi <rimpakasundi@gmail.com>
2025-09-19 16:37:02 +0530

    Add counter component with increment functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import "./App.css";
+import Counter from "./counter";
 
 function App() {
   function handleClick() {
@@ -7,10 +8,15 @@ function App() {
   const handleClick3 = () => {
     alert("clicked 3");
   };
+  const handleAdd5 = (num) => {
+    const newNum = num + 5;
+    alert(newNum);
+  };
 
   return (
     <>
       <h3>Vite + React</h3>
+      <Counter></Counter>
       {/* <button onClick="handleClick()">Click Me</button> */}
       <button onClick={handleClick}>Click Me</button>
       <button
@@ -23,6 +29,7 @@ function App() {
 
       <button onClick={handleClick3}>Click Me 3</button>
       <button onClick={() => alert("click 4")}>Click Me 4</button>
+      <button onClick={() => handleAdd5(10)}>Click add 5</button>
     </>
   );
 }
